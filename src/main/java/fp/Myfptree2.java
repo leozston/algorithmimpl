99be9961adae2d7ba93fba161834016ca459462c
u1095279116669 <1095279116669@tianchi.com>
2017-10-25 00:01:33 +0800

    fix

--- src/main/java/fp/Myfptree2.java
+++ src/main/java/fp/Myfptree2.java
@@ -82,17 +82,6 @@ public class Myfptree2 {
         return header;
     }
 
-    /**
-     * revert test1
-     * */
-
-    /**
-     * revert test2
-     * /
-     * 
-     /**
-     * master test2
-     * */
     //选择法排序,如果次数相等，则按名字排序,字典顺序,先小写后大写
     public List<TreeNode2> sort(List<TreeNode2> list){
         int len=list.size();
